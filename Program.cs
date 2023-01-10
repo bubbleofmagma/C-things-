b43c70aa94320d66258d0f2d68ade4a38d655d21
skMagma <87656578+skMagma@users.noreply.github.com>
2023-01-10 17:03:37 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+    static void main(String[] args) {
+
+    int age;
+
+        for (int i = 0; i < 10; ){
+            Console.WriteLine("age?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is " + age);
+
+            i++;
+                }
+
+            }      
+    main(args);
